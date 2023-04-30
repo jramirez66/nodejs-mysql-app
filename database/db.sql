d9493cd97bb8f92abfcafe3ac0c8d15b41ec1154
jramirez66 <juanoramirez619@gmail.com>
2023-04-30 13:51:44 -0500

    uploading project first stage

---
+++ database/db.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE database_links; 
+
+USE database_links;
+
+CREATE TABLE users(
+    id INT(11) NOT NULL,
+    username VARCHAR(16) NOT NULL, 
+    password VARCHAR(60) NOT NULL,
+    fullname VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE users
+    ADD PRIMARY KEY (id);
+
+ALTER TABLE users
+    MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
+
+DESCRIBE users;
