379fca128dda9229204b085a303f073e94079d4a
jramirez66 <juanoramirez619@gmail.com>
2023-04-30 14:30:24 -0500

    updating databases

--- database/db.sql
+++ database/db.sql
@@ -2,6 +2,7 @@ CREATE DATABASE database_links;
 
 USE database_links;
 
+-- Users Table
 CREATE TABLE users(
     id INT(11) NOT NULL,
     username VARCHAR(16) NOT NULL, 
@@ -16,3 +17,14 @@ ALTER TABLE users
     MODIFY id INT(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT = 2;
 
 DESCRIBE users;
+
+--Links Table
+CREATE TABLE links(
+    id INT (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(150) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description TEXT,
+    user_id INT(11),
+    created_at timestamp NOT NULL DEFAULT current_timestamp,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id)
+)
